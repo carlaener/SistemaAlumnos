da42beb1efe498054b3083273820e1df7b8b3167
CarlaSuarez <carlaener@gmail.com>
2024-07-02 19:38:41 -0300

    Guardado de fotos de formulario en uploads

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@
 from flask import Flask
 from flask import render_template, request, redirect 
 from flaskext.mysql import MySQL
+from datetime import datetime
 
 app = Flask(__name__)
 mysql = MySQL()
@@ -34,6 +35,15 @@ def store():
     _nombre = request.form['txtNombre']
     _correo = request.form['txtCorreo']
     _foto = request.files['txtFoto']
+
+    now = datetime.now()
+    print(now)
+    tiempo = now.strftime("%Y%H%M%S")
+    print(tiempo)
+
+    if _foto.filename != '':
+        nuevoNombreFoto = tiempo + '_' + _foto.filename
+        _foto.save("src/uploads/" + _foto.filename)
 
     sql = "INSERT INTO alumnos (nombre, correo, foto) values (%s, %s, %s);"
     datos = (_nombre, _correo, _foto.filename)
