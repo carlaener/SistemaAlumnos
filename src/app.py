153b471da9ed5923a3feb92a9be14fe0717d25bb
CarlaSuarez <carlaener@gmail.com>
2024-07-03 10:08:43 -0300

    Formulario y ruta para editar alumnos. Header y footer añadido a páginas de formularios.

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask import render_template, request, redirect 
 from flaskext.mysql import MySQL
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 mysql = MySQL()
@@ -11,6 +12,9 @@ app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = ''
 app.config['MYSQL_DATABASE_DB'] = 'alumnos'
+
+UPLOADS = os.path.join('uploads')
+app.config['UPLOADS']=UPLOADS
 
 mysql.init_app(app)
 
@@ -53,6 +57,61 @@ def store():
     conn.commit()
 
     return redirect('/')
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    sql = "DELETE FROM alumnos WHERE id=%s"
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql,id)
+    conn.commit()
+
+    return redirect('/')
+
+@app.route('/modify/<int:id>')
+def modify(id):
+    sql = "SELECT * FROM alumnos WHERE id=%s"
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql,id)
+    alumno = cursor.fetchone()
+    conn.commit()
+    return render_template('alumnos/edit.html',alumno=alumno)
+
+@app.route('/update', methods=['POST'])
+def update():
+    _nombre = request.form['txtNombre']
+    _correo = request.form['txtCorreo']
+    _foto = request.form['txtFoto']
+    id = request.form['txtId']
+
+    datos = (_nombre, _correo, id)
+
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    if _foto.filename != '':
+        now = datetime.now()
+        tiempo = now.strftime("%Y%H%M%S")
+        nuevoNombreFoto = tiempo + '_' + _foto.filename
+        _foto.save("uploads/" + nuevoNombreFoto)
+    
+    sql = "SELECT FROM alumnos WHERE id=%s"
+    cursor.execute(sql)
+    nombreFoto = cursor.fetchone()[0]
+
+    os.remove(os.path.join(app.config['UPLOADS'], nombreFoto))
+
+    sql = f'UPDATE alumnos SET nombre={_nombre}, correo={_correo} WHERE id={id}'
+
+    cursor.execute(sql)
+    conn.commit()
+
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql, datos)
+    conn.commit()
+
 
 if __name__== '__main__':
     app.run(debug=True)
